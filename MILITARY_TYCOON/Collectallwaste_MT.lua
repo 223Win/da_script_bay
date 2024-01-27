02390a8b6770561df954d3289ae162b095ad5af4
223Win <112732681+223Win@users.noreply.github.com>
2024-01-27 12:25:05 -0600

    Update Collectallwaste_MT.lua

--- MILITARY_TYCOON/Collectallwaste_MT.lua
+++ MILITARY_TYCOON/Collectallwaste_MT.lua
@@ -5,13 +5,12 @@ assert(queueonteleport,'Unsupported Env functions')
 
 local fp = fireproximityprompt
 local qot = queueonteleport
-game:HttpGet('https://raw.githubusercontent.com/223Win/da_script_bay/main/MILITARY_TYCOON/Collectallwaste_MT.lua')
 function ServerHop()
 	local Servers = game:HttpGet('https://games.roblox.com/v1/games/7180042682/servers/0?sortOrder=2&excludeFullGames=true&limit=100')
 	local DecodedServers = game:GetService("HttpService"):JSONDecode(Servers)
 	local AllServers = {}
 	
-	for i,v in pairs(DecodedServers.data) do
+	for i,v in pairs(DecodedServers.Data) do
 		table.insert(AllServers,v.id)
 	end
 	math.randomseed(math.random(1,231712))

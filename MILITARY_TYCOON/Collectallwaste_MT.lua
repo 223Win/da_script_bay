4d9ac9bfe2d76431069286de110efd802350d8d8
223Win <112732681+223Win@users.noreply.github.com>
2024-01-27 12:09:02 -0600

    Create Collectallwaste_MT.lua

---
+++ MILITARY_TYCOON/Collectallwaste_MT.lua
@@ -0,0 +1,35 @@
+
+
+assert(fireproximityprompt,'Unsupported Env functions')
+assert(queueonteleport,'Unsupported Env functions')
+
+local fp = fireproximityprompt
+local qot = queueonteleport
+
+function ServerHop()
+	local Servers = game:HttpGet('https://games.roblox.com/v1/games/7180042682/servers/0?sortOrder=2&excludeFullGames=true&limit=100')
+	local DecodedServers = game:GetService("HttpService"):JSONDecode(Servers)
+	local AllServers = {}
+	
+	for i,v in pairs(DecodedServers.Data) do
+		table.insert(AllServers,v.id)
+	end
+	math.randomseed(math.random(1,231712))
+	qot()
+	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,AllServers[math.random(1,#AllServers)],game.Players.LocalPlayer)
+end
+
+
+
+
+local WasteFolder = workspace.CurrentEventCollectables
+
+
+for i,v in pairs(WasteFolder:GetChildren()) do
+	task.wait()
+	fp(v.Hitbox.ProximityPrompt)
+end
+
+task.wait(3)
+
+ServerHop()

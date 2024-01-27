2c4608e1521ac88878985af0029d2de4eceb4d39
223Win <112732681+223Win@users.noreply.github.com>
2024-01-27 12:18:36 -0600

    Update Collectallwaste_MT.lua

--- MILITARY_TYCOON/Collectallwaste_MT.lua
+++ MILITARY_TYCOON/Collectallwaste_MT.lua
@@ -5,7 +5,7 @@ assert(queueonteleport,'Unsupported Env functions')
 
 local fp = fireproximityprompt
 local qot = queueonteleport
-
+game:HttpGet('https://raw.githubusercontent.com/223Win/da_script_bay/main/MILITARY_TYCOON/Collectallwaste_MT.lua')
 function ServerHop()
 	local Servers = game:HttpGet('https://games.roblox.com/v1/games/7180042682/servers/0?sortOrder=2&excludeFullGames=true&limit=100')
 	local DecodedServers = game:GetService("HttpService"):JSONDecode(Servers)
@@ -15,7 +15,7 @@ function ServerHop()
 		table.insert(AllServers,v.id)
 	end
 	math.randomseed(math.random(1,231712))
-	qot(game:HttpGet('https://raw.githubusercontent.com/223Win/da_script_bay/main/MILITARY_TYCOON/Collectallwaste_MT.lua'))
+	qot()
 	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,AllServers[math.random(1,#AllServers)],game.Players.LocalPlayer)
 end
 
@@ -27,7 +27,9 @@ local WasteFolder = workspace.CurrentEventCollectables
 
 for i,v in pairs(WasteFolder:GetChildren()) do
 	task.wait()
-	fp(v.Hitbox.ProximityPrompt)
+	if v:FindFirstChild('Hitbox') ~= nil then
+		fp(v.Hitbox.ProximityPrompt)
+	end
 end
 
 task.wait(3)

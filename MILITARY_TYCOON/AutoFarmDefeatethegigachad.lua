96fdbde74a299822b358d13bb3953789075bd994
223Win <112732681+223Win@users.noreply.github.com>
2024-01-28 15:38:42 -0600

    Fixed Teleport Issue on AutoFarmDefeatethegigachad.lua

--- MILITARY_TYCOON/AutoFarmDefeatethegigachad.lua
+++ MILITARY_TYCOON/AutoFarmDefeatethegigachad.lua
@@ -11,7 +11,7 @@ local PlayerConnectionAdd = game.Players.PlayerAdded
 local PlayerConnectionRem = game.Players.PlayerRemoving
 local FastMode = false
 
-warn('Version: 1.2')
+warn('Version: 1.3')
 
 -- AntiKick test
 

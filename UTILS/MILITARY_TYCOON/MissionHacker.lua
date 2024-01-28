373d08a08c94421a6d7b92174d7b89beefb983f8
223Win <112732681+223Win@users.noreply.github.com>
2024-01-28 15:23:15 -0600

    Update MissionHacker.lua

--- UTILS/MILITARY_TYCOON/MissionHacker.lua
+++ UTILS/MILITARY_TYCOON/MissionHacker.lua
@@ -16,7 +16,7 @@ function MissionHacker:UPDATE()
 		MissionHacker.C4Placements = {}
 		for i,v in pairs(MissionHacker.Stages) do
 			if v:FindFirstChild('C4DoorNew') and v:FindFirstChild('C4DoorNew'):FindFirstChild('C4') then
-				MissionHacker.C4Placements[v.Name] = v:FindFirstChild('C4DoorNew'):FindFirstChild('C4').Position
+				MissionHacker.C4Placements[tonumber(v.Name)] = v:FindFirstChild('C4DoorNew'):FindFirstChild('C4').Position
 			end
 		end
 	end

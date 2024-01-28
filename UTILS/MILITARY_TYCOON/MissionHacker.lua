699c8fe672d3f6603c24707da8f8baabc80db5d6
223Win <112732681+223Win@users.noreply.github.com>
2024-01-28 15:20:19 -0600

    Update MissionHacker.lua

--- UTILS/MILITARY_TYCOON/MissionHacker.lua
+++ UTILS/MILITARY_TYCOON/MissionHacker.lua
@@ -7,23 +7,25 @@ local Mission = game.Workspace.Mission
 
 
 
-function MissionHacker:GetStages()
-	return Mission.Stages:GetChildren()
-end
+function MissionHacker:UPDATE()
+	function MissionHacker:GetStages()
+		return Mission.Stages:GetChildren()
+	end
 
-function MissionHacker:FetchC4Placements()
-	for i,v in pairs(MissionHacker.Stages) do
-		if v:FindFirstChild('C4DoorNew') and v:FindFirstChild('C4DoorNew'):FindFirstChild('C4') then
-			MissionHacker.C4Placements[v.Name] = v:FindFirstChild('C4DoorNew'):FindFirstChild('C4').Position
+	function MissionHacker:FetchC4Placements()
+		MissionHacker.C4Placements = {}
+		for i,v in pairs(MissionHacker.Stages) do
+			if v:FindFirstChild('C4DoorNew') and v:FindFirstChild('C4DoorNew'):FindFirstChild('C4') then
+				MissionHacker.C4Placements[v.Name] = v:FindFirstChild('C4DoorNew'):FindFirstChild('C4').Position
+			end
 		end
 	end
+	
+	
+	MissionHacker.Stages = MissionHacker:GetStages()
+	
+	MissionHacker:FetchC4Placements()
 end
-
-
-MissionHacker.Stages = MissionHacker:GetStages()
-
-MissionHacker:FetchC4Placements()
-
 
 
 return MissionHacker

faf550f49e6fe6a7a4f3498fffe07db6c6769616
223Win <112732681+223Win@users.noreply.github.com>
2024-01-28 16:29:35 -0600

    Update AutoFarmDefeatethegigachad.lua

--- MILITARY_TYCOON/AutoFarmDefeatethegigachad.lua
+++ MILITARY_TYCOON/AutoFarmDefeatethegigachad.lua
@@ -11,7 +11,7 @@ local PlayerConnectionAdd = game.Players.PlayerAdded
 local PlayerConnectionRem = game.Players.PlayerRemoving
 local FastMode = false
 
-warn('Version: 1.7')
+warn('Version: 1.8')
 
 -- AntiKick test
 
